60d231495333d8138f3d2abe6c4d6cf15cbc4bf3
HeyExotic <RubixKingdom3211@gmail.com>
2025-01-27 14:12:33 -0600

    App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,5 +4,8 @@ public class App {
   public static void main(String[] args) {
 Factorizer factorizer = new Factorizer();
     System.out.println(factorizer.primeFactors(0));
+    System.out.println(factorizer.primeFacotrs(1));
+    System.out.println(factorizer.primeFactors(9));
+    System.out.println(factorizer.primeFactors(10));
   }
 }
